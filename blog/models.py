31d71fbfc608d97fdebcaea2a76569413c7f30ea
Macker85 <pmacker1985@gmail.com>
2023-04-18 20:13:53 +0000

    Add:basic page templates for 'templates' and css

--- blog/models.py
+++ blog/models.py
@@ -33,7 +33,7 @@ class Post(models.Model):
         return self.dislikes.count()
 
 
-class Comments(models.Model):
+class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=50)
