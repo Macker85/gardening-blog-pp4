5bbbccc4de46809d895fd8bf3f654f87b3aad9c9
Macker85 <pmacker1985@gmail.com>
2023-04-18 18:44:05 +0000

    Add: filter functionality to admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content',]
     summernote_fields = ('content')
+
+
+# @admin.register(Comment)
+# class CommentAdmin(admin.ModelAdmin):
