638f21cd11812b57abb99aa1ef0884a757dacd46
Macker85 <pmacker1985@gmail.com>
2023-05-12 20:17:44 +0000

    Add code and view for booking

---
+++ booking/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from crispy_forms.helper import FormHelper
+from .models import Booking
+
+# Booking form for Factime for gardening support
+
+
+class BookingForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+
+    requested_date = forms.DateField(
+        widget=forms.DateField(
+            attrs={'type': 'date', 'min': datetime.now().date()}))
+    
+    class Meta:
+        model = Booking
+        fields = (
+            'name',
+            'email',
+            'requested_date',
+            'requested_time',
+        )
--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import Booking
+from .forms import BookingForm
 
-# Create your views here.
+
+class Facetime(View):
+    template_name = 'booking/'
+    success_message = 'I look forward to chatting with you!'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            email = request.user.email
+            booking_form = BookingForm(initial={'email': email})
+        else:
+            return render(request, 'account_login')
