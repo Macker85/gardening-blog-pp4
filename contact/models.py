25638c82a01ee3df260dad353845c39fd350c9ab
Macker85 <pmacker1985@gmail.com>
2023-05-13 16:19:01 +0000

    Add:model for contact app

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Contact(models.Model):
+    """
+    a class for the Contact model
+    """
+    message_id = models.AutoField(primary_key=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="contact_user",
+        null=True
+        )
+    name = models.CharField(
+        max_length=30,
+        null=True
+        )
+    email = models.EmailField(
+        max_length=50,
+        default=""
+        )
+    phone = PhoneNumberField(null=True)
+    message = models.TextField()
+
+    class Meta:
+        ordering = ['created_date']
+
+    def __str__(self):
+        return self.name
